b498c6c2787022d84fc4baead476deb6679d29b2
Bhupesh S <bhupshs@amazon.com>
2017-12-31 13:07:32 +0530

    till step 22

---
+++ src/main/java/backend/model/UserRepository.java
@@ -0,0 +1,9 @@
+package backend.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+    List<User> findByRole(String description);
+}
